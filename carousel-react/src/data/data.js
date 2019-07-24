20ce4bc293c1ef3b72a596eaef260c6b688c396c
Louison Millet <lmillet@wisersoftware.com>
2019-07-24 18:50:31 +0200

    first commit

---
+++ carousel-react/src/data/data.js
@@ -0,0 +1,86 @@
+const data = {
+  properties: [
+    {
+      _id: "593e9297e17df20c4a237d42",
+      index: 0,
+      picture:
+        "https://cdn1.imggmi.com/uploads/2019/7/24/7e53d5d399dabaa405870db23021a3f4-full.jpg",
+      name: "Vincent Cotro",
+      poste: "Co-Founder"
+    },
+    {
+      _id: "593e9297ec4cca9c56bf61af",
+      index: 1,
+      picture:
+        "https://cdn1.imggmi.com/uploads/2019/7/24/6c46b0ad2bacc5cbd07e4f6a88d6e3fb-full.jpg",
+      name: "Alexandre",
+      poste: "Customer success"
+    },
+    {
+      _id: "593e929773c71925e5d7c11c",
+      index: 2,
+      picture:
+        "https://cdn1.imggmi.com/uploads/2019/7/24/7794e4e0228036eef53a27aef54619d6-full.jpg",
+      name: "Alexis",
+      poste: "Customer success"
+    },
+    {
+      _id: "593e92973d4d15eedb129aee",
+      index: 3,
+      picture:
+        "https://cdn1.imggmi.com/uploads/2019/7/24/dbea9acdb4721ddceba0da6a307e18a5-full.jpg",
+      name: "Clément",
+      poste: "Développeur JS"
+    },
+    {
+      _id: "593e929728b7f8543b7907e1",
+      index: 4,
+      picture:
+        "https://cdn1.imggmi.com/uploads/2019/7/24/3ab0c13343f4804b0f7f54627dad5713-full.jpg",
+      name: "Damien",
+      poste: "Co-Founder"
+    },
+    {
+      _id: "593e92975996a6263d146444",
+      index: 5,
+      picture:
+        "https://cdn1.imggmi.com/uploads/2019/7/24/9b84797208efb2fa32202a9379cb9a3f-full.jpg",
+      name: "Martin",
+      poste: "Consultant recrutement"
+    },
+    {
+      _id: "593e9297a13e246da5ba0884",
+      index: 6,
+      picture:
+        "https://cdn1.imggmi.com/uploads/2019/7/24/03eb3ab4b5951eb034da7874e73120de-full.jpg",
+      name: "Nicolas",
+      poste: "Consultant Marque Employeur"
+    },
+    {
+      _id: "593e9297afa51a013fcd07d5",
+      index: 7,
+      picture:
+        "https://cdn1.imggmi.com/uploads/2019/7/24/27cf80c98d6f3d0037b5aa34fa5b3362-full.jpg",
+      name: "Pierre",
+      poste: "Customer success"
+    },
+    {
+      _id: "593e92972bd8fd05f2ab1a89",
+      index: 8,
+      picture:
+        "https://cdn1.imggmi.com/uploads/2019/7/24/2122bfd09be5754b5959dcbcc551d8f8-full.jpg",
+      name: "Quentin",
+      poste: "Développeur React"
+    },
+    {
+      _id: "593e9297fe2e6d32c859ecf4",
+      index: 9,
+      picture:
+        "https://cdn1.imggmi.com/uploads/2019/7/24/225b9c223b93585aaad0159be8169889-full.jpg",
+      name: "Thomas",
+      poste: "Développeur React"
+    }
+  ]
+};
+
+export default data;
